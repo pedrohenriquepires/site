049dcf3df6f8ba30411dc293de45597d1989f88c
Pedro Henrique Pires <pdrhenriquepires@gmail.com>
2020-02-10 13:47:26 -0300

    wip

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const OpenBrowserPlugin = require('open-browser-webpack-plugin')
 
 const paths = require('./paths')
 
@@ -8,15 +9,34 @@ const config = {
   entry: './src/scripts/index.js',
   output: {
     path: path.resolve(paths.appRoot, 'build'),
-    filename: '[name].[contenthash].js'
+    filename: '[name].[hash].js'
   },
   devServer: {
     contentBase: path.join(__dirname, 'build'),
     compress: true,
+    hot: true,
+    inline: true,
     port: 3000
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new OpenBrowserPlugin({ url: 'http://localhost:3000' }),
+    new HtmlWebpackPlugin({ template: paths.appIndex })
+  ],
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              root: path.resolve(paths.appRoot, 'src'),
+              esModule: false
+            }
+          }
+        ]
+      },
       {
         test: /\.js$/,
         use: 'babel-loader',
@@ -28,7 +48,14 @@ const config = {
       },
       {
         test: /\.svg$/,
-        use: 'file-loader'
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false
+            }
+          }
+        ]
       },
       {
         test: /\.png$/,
@@ -36,19 +63,14 @@ const config = {
           {
             loader: 'url-loader',
             options: {
-              mimetype: 'image/png'
+              mimetype: 'image/png',
+              esModule: false
             }
           }
         ]
       }
     ]
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: paths.appIndex
-    })
-  ],
   optimization: {
     runtimeChunk: 'single',
     splitChunks: {
